421cfd78f90aa4d0738c52e12ee8f12a2eaa4046
João Pedro <jpronzanisantos@gmail.com>
2024-11-10 20:33:13 -0300

    Adicionado formula de rpc

--- src/test/java/JokenpoTest.java
+++ src/test/java/JokenpoTest.java
@@ -1,236 +1,217 @@
 import org.joao.ronzani.Jokenpo;
-import org.joao.ronzani.JokenpoRaw;
 import org.joao.ronzani.Player;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class JokenpoTest {
 
-    // primeiro jogador vence
     @Test
     public void testPedraEsmagaTesoura() {
-        Jokenpo p1 = Jokenpo.PEDRA;
-        Jokenpo p2 = Jokenpo.TESOURA;
+        Player p1 = new Player("p1", Jokenpo.PEDRA);
+        Player p2 = new Player("p2", Jokenpo.TESOURA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testPedraEsmagaLagarto() {
-        Jokenpo p1 = Jokenpo.PEDRA;
-        Jokenpo p2 = Jokenpo.LAGARTO;
+        Player p1 = new Player("p1", Jokenpo.PEDRA);
+        Player p2 = new Player("p2", Jokenpo.LAGARTO);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testPapelCobrePedra() {
-        Jokenpo p1 = Jokenpo.PAPEL;
-        Jokenpo p2 = Jokenpo.PEDRA;
+        Player p1 = new Player("p1", Jokenpo.PAPEL);
+        Player p2 = new Player("p2", Jokenpo.PEDRA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testPapelRefutaSpock() {
-        Jokenpo p1 = Jokenpo.PAPEL;
-        Jokenpo p2 = Jokenpo.SPOCK;
+        Player p1 = new Player("p1", Jokenpo.PAPEL);
+        Player p2 = new Player("p2", Jokenpo.SPOCK);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testTesouraCortaPapel() {
-        Jokenpo p1 = Jokenpo.TESOURA;
-        Jokenpo p2 = Jokenpo.PAPEL;
+        Player p1 = new Player("p1", Jokenpo.TESOURA);
+        Player p2 = new Player("p2", Jokenpo.PAPEL);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testTesouraDecapitaLagarto() {
-        Jokenpo p1 = Jokenpo.TESOURA;
-        Jokenpo p2 = Jokenpo.LAGARTO;
+        Player p1 = new Player("p1", Jokenpo.TESOURA);
+        Player p2 = new Player("p2", Jokenpo.LAGARTO);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testLagartoEnvenenaSpock() {
-        Jokenpo p1 = Jokenpo.LAGARTO;
-        Jokenpo p2 = Jokenpo.SPOCK;
+        Player p1 = new Player("p1", Jokenpo.LAGARTO);
+        Player p2 = new Player("p2", Jokenpo.SPOCK);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testLagartoComePapel() {
-        Jokenpo p1 = Jokenpo.LAGARTO;
-        Jokenpo p2 = Jokenpo.PAPEL;
+        Player p1 = new Player("p1", Jokenpo.LAGARTO);
+        Player p2 = new Player("p2", Jokenpo.PAPEL);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testSpockVaporizaPedra() {
-        Jokenpo p1 = Jokenpo.SPOCK;
-        Jokenpo p2 = Jokenpo.PEDRA;
+        Player p1 = new Player("p1", Jokenpo.SPOCK);
+        Player p2 = new Player("p2", Jokenpo.PEDRA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testSpockEsmagaTesoura() {
-        Jokenpo p1 = Jokenpo.SPOCK;
-        Jokenpo p2 = Jokenpo.TESOURA;
+        Player p1 = new Player("p1", Jokenpo.SPOCK);
+        Player p2 = new Player("p2", Jokenpo.TESOURA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p1), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p1, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
-    // segundo jogador vence
     @Test
     public void testPedraPerdeParaPapel() {
-        Jokenpo p1 = Jokenpo.PEDRA;
-        Jokenpo p2 = Jokenpo.PAPEL;
+        Player p1 = new Player("p1", Jokenpo.PEDRA);
+        Player p2 = new Player("p2", Jokenpo.PAPEL);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testPedraPerdeParaSpock() {
-        Jokenpo p1 = Jokenpo.PEDRA;
-        Jokenpo p2 = Jokenpo.SPOCK;
+        Player p1 = new Player("p1", Jokenpo.PEDRA);
+        Player p2 = new Player("p2", Jokenpo.SPOCK);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testPapelPerdeParaTesoura() {
-        Jokenpo p1 = Jokenpo.PAPEL;
-        Jokenpo p2 = Jokenpo.TESOURA;
+        Player p1 = new Player("p1", Jokenpo.PAPEL);
+        Player p2 = new Player("p2", Jokenpo.TESOURA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testPapelPerdeParaLagarto() {
-        Jokenpo p1 = Jokenpo.PAPEL;
-        Jokenpo p2 = Jokenpo.LAGARTO;
+        Player p1 = new Player("p1", Jokenpo.PAPEL);
+        Player p2 = new Player("p2", Jokenpo.LAGARTO);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testTesouraPerdeParaPedra() {
-        Jokenpo p1 = Jokenpo.TESOURA;
-        Jokenpo p2 = Jokenpo.PEDRA;
+        Player p1 = new Player("p1", Jokenpo.TESOURA);
+        Player p2 = new Player("p2", Jokenpo.PEDRA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testTesouraPerdeParaSpock() {
-        Jokenpo p1 = Jokenpo.TESOURA;
-        Jokenpo p2 = Jokenpo.SPOCK;
+        Player p1 = new Player("p1", Jokenpo.TESOURA);
+        Player p2 = new Player("p2", Jokenpo.SPOCK);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testLagartoPerdeParaPedra() {
-        Jokenpo p1 = Jokenpo.LAGARTO;
-        Jokenpo p2 = Jokenpo.PEDRA;
+        Player p1 = new Player("p1", Jokenpo.LAGARTO);
+        Player p2 = new Player("p2", Jokenpo.PEDRA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testLagartoPerdeParaTesoura() {
-        Jokenpo p1 = Jokenpo.LAGARTO;
-        Jokenpo p2 = Jokenpo.TESOURA;
+        Player p1 = new Player("p1", Jokenpo.LAGARTO);
+        Player p2 = new Player("p2", Jokenpo.TESOURA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testSpockPerdeParaPapel() {
-        Jokenpo p1 = Jokenpo.SPOCK;
-        Jokenpo p2 = Jokenpo.PAPEL;
+        Player p1 = new Player("p1", Jokenpo.SPOCK);
+        Player p2 = new Player("p2", Jokenpo.PAPEL);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
     public void testSpockPerdeParaLagarto() {
-        Jokenpo p1 = Jokenpo.SPOCK;
-        Jokenpo p2 = Jokenpo.LAGARTO;
+        Player p1 = new Player("p1", Jokenpo.SPOCK);
+        Player p2 = new Player("p2", Jokenpo.LAGARTO);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertEquals(JokenpoRaw.from(p2), winner);
-    }
-
-    // empate
-    @Test
-    public void testEmpatePedra() {
-        Jokenpo p1 = Jokenpo.PEDRA;
-        Jokenpo p2 = Jokenpo.PEDRA;
-
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertNull(winner);
+        Player winner = Jokenpo.validateWin(p1, p2);
+        Assert.assertEquals(p2, winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 
     @Test
-    public void testEmpatePapel() {
-        Jokenpo p1 = Jokenpo.PAPEL;
-        Jokenpo p2 = Jokenpo.PAPEL;
+    public void testEmpate() {
+        Player p1 = new Player("p1", Jokenpo.PEDRA);
+        Player p2 = new Player("p2", Jokenpo.PEDRA);
 
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
+        Player winner = Jokenpo.validateWin(p1, p2);
         Assert.assertNull(winner);
-    }
-
-    @Test
-    public void testEmpateTesoura() {
-        Jokenpo p1 = Jokenpo.TESOURA;
-        Jokenpo p2 = Jokenpo.TESOURA;
-
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertNull(winner);
-    }
-
-    @Test
-    public void testEmpateLagarto() {
-        Jokenpo p1 = Jokenpo.LAGARTO;
-        Jokenpo p2 = Jokenpo.LAGARTO;
-
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertNull(winner);
-    }
-
-    @Test
-    public void testEmpateSpock() {
-        Jokenpo p1 = Jokenpo.SPOCK;
-        Jokenpo p2 = Jokenpo.SPOCK;
-
-        JokenpoRaw winner = Jokenpo.validateWin(p1, p2);
-        Assert.assertNull(winner);
+        Assert.assertEquals(Jokenpo.winMessage(winner, p2.getJogada()), "Pedra amassa tesoura");
     }
 }
